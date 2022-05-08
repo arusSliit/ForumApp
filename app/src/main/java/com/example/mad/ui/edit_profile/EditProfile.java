df74db45b00f1026ec87e9612ad4116cae8659d0
RGinorajh <it20654894@my.sliit.lk>
2022-05-08 17:44:14 +0530

    Mad_mobile_app_project

--- app/src/main/java/com/example/mad/ui/edit_profile/EditProfile.java
+++ app/src/main/java/com/example/mad/ui/edit_profile/EditProfile.java
@@ -7,10 +7,15 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.mad.R;
 import com.example.mad.databinding.ActivityEditProfileBinding;
 import com.example.mad.user.User;
 import com.example.mad.user_profile.UserProfileDB;
@@ -22,6 +27,8 @@ public class EditProfile extends AppCompatActivity {
     ProgressDialog pd;
     ActivityEditProfileBinding binding;
 
+    EditText etMail = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,7 @@ public class EditProfile extends AppCompatActivity {
         binding.userEmail.setText(User.getEmail());
         pd = new ProgressDialog(this);
         pd.setMessage("Uploading....");
+        etMail = findViewById(R.id.user_password);
 
         binding.chooseImg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +56,29 @@ public class EditProfile extends AppCompatActivity {
                 UserProfileDB.uploadProfilePic(filePath,pd,EditProfile.this);
                 User.updateDisplayName(binding.userName.getText().toString());
                 User.updateEmail(binding.userEmail.getText().toString());
+                emailValidator(etMail);
+
                 if(!binding.userPassword.getText().toString().isEmpty()){
                     User.updatePassword(binding.userPassword.getText().toString());
                 }
             }
         });
+    }
+
+    public void emailValidator(EditText etMail) {
+
+        // extract the entered data from the EditText
+        String emailToText = etMail.getText().toString();
+
+        // Android offers the inbuilt patterns which the entered
+        // data from the EditText field needs to be compared with
+        // In this case the the entered data needs to compared with
+        // the EMAIL_ADDRESS, which is implemented same below
+        if (!emailToText.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailToText).matches()) {
+            Toast.makeText(this, "Email Verified !", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Enter valid Email address !", Toast.LENGTH_SHORT).show();
+        }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
